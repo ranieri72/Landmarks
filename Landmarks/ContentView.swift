97bd7b2914244599bbeb9f869b0b513771d4c712
Ranieri Aguiar <ranieri.aguiar@trinitysolucoes.com>
2019-11-22 15:32:06 -0300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ranieri Aguiar on 22/11/19.
+//  Copyright © 2019 Ranieri Aguiar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
